2d80dd64273130f9a2bcf85854daa0a174f92a7c
eekatzee18 <kuptsova.98@list.ru>
2026-01-15 13:09:19 +0400

    resolved mistake

--- src/js/basic.js
+++ src/js/basic.js
@@ -1,11 +1,3 @@
-export default function sum(items) {
-  let result = 0;
-  for (const item of items) {
-    result += item;
-  }
-  return result;
-}
-
-export function SortHealth(heroes){
-  return heroes.sort((a,b) => b.health - a.health);
+export default function SortHealth(heroes) {
+  return heroes.sort((a, b) => b.health - a.health);
 }
